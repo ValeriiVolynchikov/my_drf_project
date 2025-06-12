04d8f5b4c3bef709e221919711afe81413beb253
valerii volynchikov <avto162@gmail.com>
2025-06-12 17:15:57 +0300

    миграции

--- config/settings.py
+++ config/settings.py
@@ -97,4 +97,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'Users.User'
+AUTH_USER_MODEL = 'users.User'
--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название курса", help_text="Введите название курса"
+    )
+    preview = models.ImageField(upload_to="lms/preview_course", blank=True, null=True)
+    description = models.CharField(
+        max_length=50,
+        verbose_name="Описание курса",
+        help_text="Введите описание курса",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название урока", help_text="Введите название урока"
+    )
+    preview = models.ImageField(upload_to="lms/preview_lesson", blank=True, null=True)
+    description = models.CharField(
+        max_length=50,
+        verbose_name="Описание урока",
+        help_text="Введите описание урока",
+        blank=True,
+        null=True,
+    )
+    video_url = models.CharField(
+        verbose_name="Ссылка на видео",
+        help_text="Введите ссылку на видеоурок",
+        blank=True,
+        null=True,
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Курс", help_text="Выберите курс"
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
